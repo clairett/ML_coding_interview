554e6021db2bf87a5d2f6deabfe4884173de4929
Tian Tian <tian.tianclaire@gmail.com>
2025-05-15 11:21:52 -0400

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -14,7 +14,9 @@ class KNearestNeighbors:
         return np.array([self._predict(x) for x in X])
 
     def _predict(self, x):
+        # axis=1 tells NumPy: "operate across columns for each row
         distances = np.linalg.norm(self.X_train - x, axis=1)
+        
         k_indices = np.argsort(distances)[:self.k]
         k_labels = self.y[k_indices]
         most_common = Counter(k_labels).most_common(1)
@@ -26,13 +28,13 @@ if __name__ == "__main__":
     # Sample training data
     X_train = [[1, 2], [2, 3], [3, 4], [6, 7], [7, 8]]
     y_train = [0, 0, 0, 1, 1]
-    
+
     # Create and train classifier
     knn = KNearestNeighbors(k=3)
     knn.fit(X_train, y_train)
-    
+
     # Sample test data
     X_test = [[2, 2], [6, 6]]
     predictions = knn.predict(X_test)
-    
+
     print("Predictions:", predictions)
